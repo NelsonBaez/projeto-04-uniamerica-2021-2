f4b35a030521fb103bbcdbe4cd01b25c0b3df964
Nelson J S Baez <bluexbaez@gmail.com>
2021-12-15 10:35:24 -0300

    Feito teste unidade caso nao de erro

--- backend/src/test/java/com/example/projeto04/service/ProductServiceTest.java
+++ backend/src/test/java/com/example/projeto04/service/ProductServiceTest.java
@@ -3,6 +3,7 @@ package com.example.projeto04.service;
 import com.example.projeto04.exceptions.QuantityException;
 import com.example.projeto04.model.Order;
 import com.example.projeto04.model.Product;
+import com.example.projeto04.model.Purchase;
 import com.example.projeto04.model.Supplier;
 import com.example.projeto04.repository.ProductRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -34,6 +35,31 @@ class ProductServiceTest {
         productService = new ProductService(productRepository, supplierService);
     }
 
+    @Test
+    void shouldDecreaseCurrentStockWithOrder(){
+        Order order = Order.builder()
+                .quantity(2)
+                .build();
+
+        Purchase purchase = Purchase.builder()
+                .quantity(3)
+                .build();
+        ArrayList purchases = new ArrayList();
+        purchases.add(purchase);
+
+        Product product = Product.builder()
+                .name("caneta")
+                .orders(new ArrayList<>())
+                .purchases(purchases)
+                .build();
+
+        Mockito.when(productRepository.findById(1L)).thenReturn(Optional.of(product));
+        Mockito.when(productRepository.save(product)).thenReturn(product);
+
+        var result = productService.order(1L, order);
+
+        assertThat(result.getCurrentStock()).isEqualTo(1);
+    }
 
     @Test
     void shouldReturnExceptionIfCurrentStockIsLessThanQuantityToOrder() {
